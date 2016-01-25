742cbf2c39db73c5df6b6642eeff591ec1aa33f3
Dan Swain <dan.t.swain@gmail.com>
2016-01-25 17:29:32 -0500

    Split things into their own files

---
+++ lib/thrash/type.ex
@@ -0,0 +1,4 @@
+defmodule Thrash.Type do
+  def id(:i32), do: 8
+  def id(:string), do: 11
+end
--- test/thrash_test.exs
+++ test/thrash_test.exs
@@ -2,25 +2,4 @@ defmodule ThrashTest do
   use ExUnit.Case
   doctest Thrash
 
-  defmodule SimpleStruct do
-    defstruct id: nil, name: nil
-
-    require Thrash.BinaryAcceleratedProtocol
-    Thrash.BinaryAcceleratedProtocol.generate_deserializer(id: :i32, name: :string)
-    Thrash.BinaryAcceleratedProtocol.generate_serializer(id: :i32, name: :string)
-  end
-
-  test "deserializes a struct" do
-    str = "\b\x00\x01\x00\x00\x00*\v\x00\x02\x00\x00\x00\bmy thing\x00"
-    expected = %ThrashTest.SimpleStruct{id: 42, name: "my thing"}
-    deserialized = ThrashTest.SimpleStruct.deserialize(str)
-    assert(deserialized == expected)
-  end
-
-  test "serializes a struct" do
-    struct = %ThrashTest.SimpleStruct{id: 42, name: "my thing"}
-    expected = "\b\x00\x01\x00\x00\x00*\v\x00\x02\x00\x00\x00\bmy thing\x00"
-    serialized = ThrashTest.SimpleStruct.serialize(struct)
-    assert(serialized == expected)
-  end
 end

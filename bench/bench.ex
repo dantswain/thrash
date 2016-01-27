f63455804f4be2430ba30afdeb2663dc28b296f0
Dan Swain <dan.t.swain@gmail.com>
2016-01-27 14:59:52 -0500

    Update benchmark, still showing significant speedup

--- bench/bench.ex
+++ bench/bench.ex
@@ -1,16 +1,38 @@
 defmodule Bench do
-  defmodule ThrashSimpleStruct do
-    defstruct id: nil, name: nil
+  defmodule TestSubStruct do
+    defstruct sub_id: nil, sub_name: nil
 
     require Thrash.Protocol.Binary
-    Thrash.Protocol.Binary.generate_serializer(id: :i32, name: :string)
-    Thrash.Protocol.Binary.generate_deserializer(id: :i32, name: :string)
+    Thrash.Protocol.Binary.generate_deserializer(sub_id: :i32, sub_name: :string)
+    Thrash.Protocol.Binary.generate_serializer(sub_id: :i32, sub_name: :string)
+  end
+
+  defmodule TestStruct do
+    defstruct id: nil, name: nil, list_of_ints: [], bigint: nil, sub_struct: %TestSubStruct{}
+
+    require Thrash.Protocol.Binary
+    Thrash.Protocol.Binary.generate_deserializer(id: :i32,
+                                                 name: :string,
+                                                 list_of_ints: {:list, :i32},
+                                                 bigint: :i64,
+                                                 sub_struct: {:struct, TestSubStruct})
+    Thrash.Protocol.Binary.generate_serializer(id: :i32,
+                                               name: :string,
+                                               list_of_ints: {:list, :i32},
+                                               bigint: :i64,
+                                               sub_struct: {:struct, TestSubStruct})
   end
 
   def bench_thrift_ex() do
+    require SubStruct
     require SimpleStruct
 
-    s = SimpleStruct.simple_struct(id: 42, name: "my thing")
+    sub = SubStruct.sub_struct(sub_id: 77, sub_name: "sub thing")
+    s = SimpleStruct.simple_struct(id: 42,
+                                   name: "my thing",
+                                   list_of_ints: [4, 8, 15, 16, 23, 42],
+                                   bigint: -9999999999999,
+                                   sub_struct: sub)
     str = ThriftEx.to_binary(s, SimpleStruct.struct_info)
 
     Benchwarmer.benchmark(fn ->
@@ -23,15 +45,21 @@ defmodule Bench do
   end
 
   def bench_thrash() do
-    s = %ThrashSimpleStruct{id: 42, name: "my thing"}
-    str = ThrashSimpleStruct.serialize(s)
+    sub = %TestSubStruct{sub_id: 77, sub_name: "a sub struct"}
+    s = %TestStruct{id: 42,
+                    name: "my thing",
+                    list_of_ints: [4, 8, 15, 16, 23, 42],
+                    bigint: -9999999999999,
+                    sub_struct: sub}
+
+    str = TestStruct.serialize(s)
 
     Benchwarmer.benchmark(fn ->
-      ThrashSimpleStruct.serialize(s)
+      TestStruct.serialize(s)
     end)
 
     Benchwarmer.benchmark(fn ->
-      ThrashSimpleStruct.deserialize(str)
+      TestStruct.deserialize(str)
     end)
   end
 end

8ec45b72901b2192e61929b357b043d9b655b2b7
Dan Swain <dan.t.swain@gmail.com>
2016-02-02 19:30:25 -0500

    Implement generic deserializer
    Still want to figure out generic list deserializer

--- lib/thrash/protocol/binary.ex
+++ lib/thrash/protocol/binary.ex
@@ -1,6 +1,58 @@
 defmodule Thrash.Protocol.Binary do
   alias Thrash.Type
 
+  def header(type, ix) do
+    quote do
+      << unquote(Type.id(type)), unquote(ix) + 1 :: 16-unsigned >>
+    end
+  end
+
+  def value_serializer(:bool, var) do
+    quote do
+      << Thrash.Protocol.Binary.bool_to_byte(unquote(Macro.var(var, __MODULE__))) :: 8-unsigned >>
+    end
+  end
+  def value_serializer(:double, var) do
+    quote do
+      << unquote(Macro.var(var, __MODULE__)) :: signed-float >>
+    end
+  end
+  def value_serializer(:i32, var) do
+    quote do
+      << unquote(Macro.var(var, __MODULE__)) :: 32-signed >>
+    end
+  end
+  def value_serializer({:enum, enum_module}, var) do
+    quote do
+      << unquote(enum_module).id(unquote(Macro.var(var, __MODULE__))) :: 32-unsigned >>
+    end
+  end
+  def value_serializer(:i64, var) do
+    quote do
+      << unquote(Macro.var(var, __MODULE__)) :: 64-signed >>
+    end
+  end
+  def value_serializer(:string, var) do
+    quote do
+      << byte_size(unquote(Macro.var(var, __MODULE__))) :: 32-unsigned,
+      unquote(Macro.var(var, __MODULE__)) :: binary >>
+    end
+  end
+  def value_serializer({:struct, struct_module}, var) do
+    quote do
+      unquote(struct_module).serialize(unquote(Macro.var(var, __MODULE__)))
+    end
+  end
+  def value_serializer({:list, of_type}, var) do
+    quote do
+      << unquote(Type.id(of_type)),
+      length(unquote(Macro.var(var, __MODULE__))) :: 32-unsigned >> <>
+      (Enum.map(unquote(Macro.var(var, __MODULE__)),
+            fn(v) -> unquote(value_serializer(of_type, :v)) end)
+       |> Enum.join)
+    end
+  end
+
   def bool_to_byte(true), do: 1
   def bool_to_byte(false), do: 0
 
@@ -8,7 +60,7 @@ defmodule Thrash.Protocol.Binary do
   def byte_to_bool(0), do: false
 
   def deserialize_list(_, 0, {acc, str}) do
-    {acc, str}
+    {Enum.reverse(acc), str}
   end
   def deserialize_list(:i32, len, {acc, str}) do
     << value :: 32-signed, rest :: binary >> = str
@@ -76,130 +128,84 @@ defmodule Thrash.Protocol.Binary do
     end)
   end
 
-  def deserializer(:bool, fieldname, ix) do
-    quote do
-      def deserialize_field(<< unquote(Type.id(:bool)), unquote(ix + 1) :: 16-unsigned, value :: 8-unsigned, rest :: binary >>,
-                            acc) do
-        value = Thrash.Protocol.Binary.byte_to_bool(value)
-        deserialize_field(rest, Map.put(acc, unquote(fieldname), value))
-      end
-    end
-  end
-  def deserializer(:double, fieldname, ix) do
-    quote do
-      def deserialize_field(<< unquote(Type.id(:double)), unquote(ix + 1) :: 16-unsigned, value :: signed-float, rest :: binary >>,
-                            acc) do
-        deserialize_field(rest, Map.put(acc, unquote(fieldname), value))
-      end
-    end
-  end
-  def deserializer(:i32, fieldname, ix) do
-    quote do
-      def deserialize_field(<< unquote(Type.id(:i32)), unquote(ix + 1) :: 16-unsigned, value :: 32-signed, rest :: binary>>,
-                            acc) do
-        deserialize_field(rest, Map.put(acc, unquote(fieldname), value))
-      end
-    end
-  end
-  def deserializer({:enum, enum_module}, fieldname, ix) do
-    quote do
-      def deserialize_field(<< unquote(Type.id(:enum)), unquote(ix + 1) :: 16-unsigned, value :: 32-signed, rest :: binary >>,
-                            acc) do
-        value = unquote(enum_module).atom(value)
-        deserialize_field(rest, Map.put(acc, unquote(fieldname), value))
-      end
-    end
-  end
-  def deserializer(:i64, fieldname, ix) do
-    quote do
-      def deserialize_field(<< unquote(Type.id(:i64)), unquote(ix + 1) :: 16-unsigned, value :: 64-signed, rest :: binary>>,
-                            acc) do
-        deserialize_field(rest, Map.put(acc, unquote(fieldname), value))
-      end
-    end
-  end
-  def deserializer(:string, fieldname, ix) do
-    quote do
-      def deserialize_field(<< unquote(Type.id(:string)), unquote(ix + 1) :: 16-unsigned, len :: 32-unsigned, value :: size(len)-binary, rest :: binary>>,
-                            acc) do
-        deserialize_field(rest, Map.put(acc, unquote(fieldname), value))
-      end
-    end
-  end
-  def deserializer({:struct, struct_module}, fieldname, ix) do
-    quote do
-      def deserialize_field(<< unquote(Type.id(:struct)), unquote(ix + 1) :: 16-unsigned, rest :: binary >>,
-                            acc) do
-        {sub_struct, rest} = unquote(struct_module).deserialize(rest)
-        deserialize_field(rest, Map.put(acc, unquote(fieldname), sub_struct))
-      end
-    end
-  end
-  def deserializer({:list, of_type}, fieldname, ix) do
-    quote do
-      def deserialize_field(<< unquote(Type.id(:list)), unquote(ix + 1) :: 16-unsigned, unquote(Type.id(of_type)), len :: 32-unsigned, rest :: binary >>,
-                            acc) do
-        {list, rest} = Thrash.Protocol.Binary.deserialize_list(unquote(of_type), len, {[], rest})
-        deserialize_field(rest, Map.put(acc, unquote(fieldname), Enum.reverse(list)))
-      end
-    end
-  end
   def deserializer(nil, :final, _ix) do
     quote do
       def deserialize_field(<< 0, remainder :: binary >>, acc), do: {acc, remainder}
     end
   end
-
-  def header(type, ix) do
+  def deserializer(type, fieldname, ix) do
     quote do
-      << unquote(Type.id(type)), unquote(ix) + 1 :: 16-unsigned >>
+      def deserialize_field(
+            unquote(splice_binaries(header(type, ix),
+                                    value_matcher(type, :value))
+                    |> splice_binaries(quote do: << rest :: binary >>)), acc) do
+        {value, rest} = unquote(value_mapper(type, :value, :rest))
+        deserialize_field(rest, Map.put(acc,
+                                        unquote(fieldname),
+                                        value))
+      end
     end
   end
 
-  def value_serializer(:bool, var) do
+  def value_matcher(:bool, var) do
     quote do
-      << Thrash.Protocol.Binary.bool_to_byte(unquote(Macro.var(var, __MODULE__))) :: 8-unsigned >>
+      << unquote(Macro.var(var, __MODULE__)) :: 8-unsigned >>
     end
   end
-  def value_serializer(:double, var) do
-    quote do
-      << unquote(Macro.var(var, __MODULE__)) :: signed-float >>
-    end
-  end
-  def value_serializer(:i32, var) do
+  def value_matcher({:enum, _enum_module}, var) do
     quote do
       << unquote(Macro.var(var, __MODULE__)) :: 32-signed >>
     end
   end
-  def value_serializer({:enum, enum_module}, var) do
+  def value_matcher(:string, var) do
     quote do
-      << unquote(enum_module).id(unquote(Macro.var(var, __MODULE__))) :: 32-unsigned >>
+      << len :: 32-unsigned, unquote(Macro.var(var, __MODULE__)) :: size(len)-binary >>
     end
   end
-  def value_serializer(:i64, var) do
+  def value_matcher({:struct, _struct_module}, var) do
     quote do
-      << unquote(Macro.var(var, __MODULE__)) :: 64-signed >>
+      << >>
     end
   end
-  def value_serializer(:string, var) do
+  def value_matcher({:list, of_type}, var) do
+    # "var" will be the length of the list
     quote do
-      << byte_size(unquote(Macro.var(var, __MODULE__))) :: 32-unsigned,
-      unquote(Macro.var(var, __MODULE__)) :: binary >>
+      << unquote(Type.id(of_type)), unquote(Macro.var(var, __MODULE__)) :: 32-unsigned >>
     end
   end
-  def value_serializer({:struct, struct_module}, var) do
+  def value_matcher(type, var) do
+    # for "simple" values, we can use the same pattern that value_serializer generates
+    value_serializer(type, var)
+  end
+
+  def value_mapper(:bool, var, rest) do
     quote do
-      unquote(struct_module).serialize(unquote(Macro.var(var, __MODULE__)))
+      {Thrash.Protocol.Binary.byte_to_bool(unquote(Macro.var(var, __MODULE__))),
+       unquote(Macro.var(rest, __MODULE__))}
     end
   end
-  def value_serializer({:list, of_type}, var) do
+  def value_mapper({:enum, enum_module}, var, rest) do
     quote do
-      << unquote(Type.id(of_type)),
-      length(unquote(Macro.var(var, __MODULE__))) :: 32-unsigned >> <>
-      (Enum.map(unquote(Macro.var(var, __MODULE__)),
-            fn(v) -> unquote(value_serializer(of_type, :v)) end)
-       |> Enum.join)
+      {unquote(enum_module).atom(unquote(Macro.var(var, __MODULE__))),
+       unquote(Macro.var(rest, __MODULE__))}
     end
+  end
+  def value_mapper({:struct, struct_module}, _var, rest) do
+    quote do
+      unquote(struct_module).deserialize(unquote(Macro.var(rest, __MODULE__)))
+    end
+  end
+  def value_mapper({:list, of_type}, var, rest) do
+    quote do
+      Thrash.Protocol.Binary.deserialize_list(unquote(of_type),
+                                              unquote(Macro.var(var, __MODULE__)),
+                                              {[], unquote(Macro.var(rest, __MODULE__))})
+    end
+  end
+  def value_mapper(_type, val, rest) do
+    # note the tuple is the same as its quoted value, so we don't need
+    # to quote/unquote here
+    {Macro.var(val, __MODULE__), Macro.var(rest, __MODULE__)}
   end
 
   def empty_value?({:struct, struct_module}) do

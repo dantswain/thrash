f71e35103d17fccad6bcf4e90a321ca93d5d47be
Dan Swain <dan.t.swain@gmail.com>
2016-10-19 17:03:20 -0700

    Don't set idl files outside test env

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,11 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+# If this ever gets more complex, switch to separate .exs files
+defmodule Config do
+  def idl_files(:test), do: Path.wildcard("test/thrift/**/*.thrift")
+  def idl_files(_), do: []
+end
+
 config :thrash,
-  idl_files: Path.wildcard("test/thrift/**/*.thrift")
+  idl_files: Config.idl_files(Mix.env)

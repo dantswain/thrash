a46cba5df995416bb49a87b5f5252a1905076599
Dan Swain <dan.t.swain@gmail.com>
2016-12-14 11:22:47 -0500

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Thrash.Mixfile do
 
   def project do
     [app: :thrash,
-     version: "0.3.0",
+     version: "0.3.1",
      description: description,
      package: package,
      elixir: "~> 1.1",
